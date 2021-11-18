bdd0e15d024c90ec761ae5310b9fd0c0a59f2ba3
Martin Hodler <martin.hodler@gmail.com>
2021-11-19 00:08:22 +0100

    enhanced melee attack and setup fmod audio listener for camera controller

--- ProductionDays/Assets/Scripts/AI/States/Boss/BossMelee.cs
+++ ProductionDays/Assets/Scripts/AI/States/Boss/BossMelee.cs
@@ -7,64 +7,90 @@ using UnityEngine;
 public class BossMelee : State
 {
     private GameObject bossFist;
-    private Vector3 bossFistPosition;
+    private Vector3 bossPosition;
     private CameraManager cameraManager;
     private Vector3 bossFistGroundPosition;
     private GameObject effect;
     private float attackGroundY;
+    private Vector3 attackPosition;
+
+    private PlayerController playerController;
+    private Renderer[] renderers;
+    private bool updateEffectPosition = false;
     
     public override void OnStart()
     {
         cameraManager = InstanceRepository.Instance.Get<CameraManager>();
-        
+
+        bossPosition = GameObject.transform.position;
         bossFist = GameObject.transform.Find("BossFist").gameObject;
-        bossFistPosition = bossFist.transform.position;
 
+        renderers = GameObject.GetComponentsInChildren<Renderer>();
 
-        float floorY = InstanceRepository.Instance.Get<PlayerController>().GetFloorPosition().y;
-        Debug.Log(floorY);
+        playerController = InstanceRepository.Instance.Get<PlayerController>();
+        float floorY = playerController.GetFloorPosition().y;
         // var renderer = GameObject.GetComponent<Renderer>();
         // if (renderer == null)
         //     renderer = GameObject.GetComponentInChildren<Renderer>();
         //     
         // float bossHalfHeight = renderer.bounds.extents.y;
-        bossFistGroundPosition = bossFistPosition;
+        bossFistGroundPosition = GameObject.transform.position;
         bossFistGroundPosition.y = floorY + 0.1f;
 
         attackGroundY = floorY + BossConfig.MeleeHitHeight;
         
         StartCoroutine(Melee());
     }
-    
+
+    public override void Update()
+    {
+        if (updateEffectPosition)
+        {
+            effect.transform.position = Vector3.Lerp(effect.transform.position, playerController.transform.position + new Vector3(0, 0.1f, 0), Time.deltaTime * 6f);
+        }
+    }
+
     private IEnumerator Melee()
     {
-        effect = GameObject.Instantiate(BossConfig.MeleeEffect, bossFistGroundPosition, Quaternion.identity);
+        GameObject.transform.DOMoveY(attackGroundY + 20f, 1f);
         
+        effect = GameObject.Instantiate(BossConfig.MeleeEffect, bossFistGroundPosition, Quaternion.identity);
+        updateEffectPosition = true;
         
         yield return new WaitForSeconds(2.5f);
+        updateEffectPosition = false;
+        Vector3 pos = effect.transform.position;
+        pos.y = attackGroundY + 20f;
+        bossFist.transform.position = pos; 
+        EnableRenderers(false);
+        yield return new WaitForSeconds(0.05f);
         bossFist.SetActive(true);
         yield return new WaitForSeconds(.6f);
 
         // Vector3 goalPos = bossFistPosition + (Vector3.down * BossConfig.MeleeHitHeight);
-        AudioManager.Instance.PlayOneShot(AudioEvent.Combat.BossStomp, GameObject.transform.position);
+        AudioManager.Instance.PlayOneShot(AudioEvent.Combat.BossStomp, effect.transform.position);
 
         bossFist.transform.DOMoveY(attackGroundY, 0.25f).SetEase(Ease.InCubic);
         yield return new WaitForSeconds(0.25f);
-        // while (Vector3.Distance(bossFist.transform.position, goalPos) > 0.1f)
-        // {
-        //     bossFist.transform.position =
-        //         Vector3.Lerp(bossFist.transform.position, goalPos, Time.deltaTime * 20);
-        //
-        //     yield return null;
-        // }
 
         cameraManager.ScreenShake(0.3f);
         yield return new WaitForSeconds(2f);
-        bossFist.SetActive(false);
-        bossFist.transform.position = bossFistPosition;
         
+        bossFist.SetActive(false);
+        GameObject.transform.position = bossPosition;
+
+        yield return new WaitForSeconds(0.05f);
+        EnableRenderers(true);
         GameObject.Destroy(effect);
         SetState(new BossDecision());
+    }
+
+    private void EnableRenderers(bool enabled)
+    {
+        foreach (var renderer in renderers)
+        {
+            renderer.enabled = enabled;
+        }
     }
 
     public override void OnEnd()
